3f7e083a65135214d22450adfb9dbc3e464f497d
Meri <47123315+MerryMeri@users.noreply.github.com>
2020-10-23 23:45:52 -0400

    Add files via upload

---
+++ decryption.py
@@ -0,0 +1,38 @@
+import argparse
+
+def main():
+    cyphertext = input("Please put cyphertext here: ")
+    cypherlist = cyphertext.split(",")
+    keyspace = {}
+
+    plaintext_flag = True
+    plaintext = ""
+
+    plaintext_candidates = []
+    plaintext_candidates.append("underwaists wayfarings fluty analgia refuels transcribing nibbled okra buttonholer venalness hamlet praus apprisers presifted cubital walloper dissembler bunting wizardries squirrel preselect befitted licensee encumbrances proliferations tinkerer egrets recourse churl kolinskies ionospheric docents unnatural scuffler muches petulant acorns subconscious xyster tunelessly boners slag amazement intercapillary manse unsay embezzle stuccoer dissembles batwing valediction iceboxes ketchups phonily con")
+    plaintext_candidates.append("rhomb subrents brasiers render avg tote lesbian dibbers jeopardy struggling urogram furrowed hydrargyrum advertizing cheroots goons congratulation assaulters ictuses indurates wingovers relishes briskly livelihoods inflatable serialized lockboxes cowers holster conciliating parentage yowing restores conformities marted barrettes graphically overdevelop sublimely chokey chinches abstracts rights hockshops bourgeoisie coalition translucent fiascoes panzer mucus capacitated stereotyper omahas produ")
+    plaintext_candidates.append("yorkers peccaries agenda beshrews outboxing biding herons liturgies nonconciliatory elliptical confidants concealable teacups chairmanning proems ecclesiastically shafting nonpossessively doughboy inclusion linden zebroid parabolic misadventures fanciers grovelers requiters catmints hyped necklace rootstock rigorously indissolubility universally burrowers underproduced disillusionment wrestling yellowbellied sherpa unburnt jewelry grange dicker overheats daphnia arteriosclerotic landsat jongleur")
+    plaintext_candidates.append("cygnets chatterers pauline passive expounders cordwains caravel antidisestablishmentarianism syllabubs purled hangdogs clonic murmurers admirable subdialects lockjaws unpatentable jagging negotiated impersonates mammons chumminess semi pinner comprised managership conus turned netherlands temporariness languishers aerate sadists chemistry migraine froggiest sounding rapidly shelving maligning shriek faeries misogynist clarities oversight doylies remodeler tauruses prostrated frugging comestible ")
+    plaintext_candidates.append("ovulatory geriatric hijack nonintoxicants prophylactic nonprotective skyhook warehouser paganized brigading european sassier antipasti tallyho warmer portables selling scheming amirate flanker photosensitizer multistage utile paralyzes indexer backrests tarmac doles siphoned casavas mudslinging nonverbal weevil arbitral painted vespertine plexiglass tanker seaworthiness uninterested anathematizing conduces terbiums wheelbarrow kabalas stagnation briskets counterclockwise hearthsides spuriously s")
+
+    for line in plaintext_candidates:
+        for i in range(0, len(line), 1):
+            if (plaintext_flag == False): break
+            if not (line[i] in keyspace):
+                for j in range(i+1, len(line), 1):
+                    if (cypherlist[j] == cypherlist[i]):
+                        keyspace[cypherlist[i]] = line[i]
+                        if not (line[j] == line[i]):
+                            plaintext_flag = False
+                            break
+        if (plaintext_flag == True):
+            plaintext = line
+            break
+        else:
+            plaintext_flag = True
+            keyspace = {}
+
+    print(plaintext)
+
+if __name__ == "__main__":
+    main()
